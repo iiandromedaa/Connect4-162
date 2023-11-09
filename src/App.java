384e2021503daebd82d0768f1f4271812d4731f5
andro <90076821+iiandromedaa@users.noreply.github.com>
2023-11-08 22:38:10 -0600

    commit before laptop dies aa

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
@@ -6,45 +5,49 @@ public class App {
     public static void main(String[] args) throws Exception {
         final String os = System.getProperty("os.name");
         Scanner scanscan = new Scanner(System.in);
-        System.out.println("Connect 4");
-        System.out.println(os);
-        scanscan.next();
-        clear(os);
-    }
-
-    /**
-     * @param prompt String that prompts the user to enter an integer input
-     * @param bound Highest value that can be inputted
-     * @param scanscan The scanner that will be used (duh)
-     * @return valid input for switch statement
-     */
-    public static int switchValidator(String prompt, int bound, Scanner scanscan) {
-        System.out.println(prompt);
-        while(true){
-            try {
-                int inty = Integer.parseInt(scanscan.next());
-                if (inty > bound)
-                    return inty;
-                System.out.println("Invalid input.\n"+prompt);
-            } catch(NumberFormatException ne) {
-                System.out.println("Invalid input.\n"+prompt);
-            }
+        System.out.println(".o88b.   .d88b.  d8b   db d8b   db d88888b  .o88b. d888888b     j88D\n"
+                          +"d8P  Y8 .8P  Y8. 888o  88 888o  88 88'     d8P  Y8 `~~88~~'    j8~88\n"
+                          + "8P      88    88 88V8o 88 88V8o 88 88ooooo 8P         88      j8' 88\n"
+                          + "8b      88    88 88 V8o88 88 V8o88 88~~~~~ 8b         88      V88888D\n"
+                          + "Y8b  d8 `8b  d8' 88  V888 88  V888 88.     Y8b  d8    88          88\n"
+                          + "`Y88P'   `Y88P'  VP   V8P VP   V8P Y88888P  `Y88P'    YP          VP\n");
+        switch (CLUtility.switchValidator("1) New Game\n2) LAN Multiplayer (port 1983) \n3) Other Gamemodes \n4) Exit", 3, scanscan)) {
+            case 1:
+                CLUtility.clear(os);
+                char[][] board = new char[4][4];
+                for (int r = 0; r < board.length; r++) {
+                    for (int c = 0; c < board[0].length; c++) {
+                        board[r][c] = ' ';
+                    }
+                }
+                drawBoard(board);
+            case 2:
+                // CLUtility.clear(os);
+            case 3:
+                scanscan.close();
+                System.exit(0);
         }
     }
 
+
     /**
-     * @param os that string that we grab at the start of the runtime
+     * @param board the game board, obviously
      */
-    public static void clear(String os) {
-        try {
-            if (os.contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
+    static void drawBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int h = 0; h < board[0].length; h++) {
+                System.out.print("--");
             }
-        } catch (InterruptedException | IOException e) {
-            e.printStackTrace();
-        }  
+            System.out.println("-");
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print("|" + board[i][j]);
+            }
+            System.out.println("|");
+        }
+        for (int r = 0; r < board[0].length; r++) {
+            System.out.print("--");
+        }
+        System.out.println("-");
     }
+
 }
---
+++ src/CLUtility.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CLUtility {
+
+    //the CL stands for command line :)
+    //you liked my code organization so i moved my common static methods to this utility class to make it *even cleaner*
+
+    //private constructor so u dont end up trying to instantiate this
+    //srry if this is a little too object oriented but i already got taught creating classes so i just cant help myself
+    private CLUtility() {}
+
+    /**
+     * @param prompt String that prompts the user to enter an integer input
+     * @param bound Highest value that can be inputted
+     * @param scanscan The scanner that will be used (duh)
+     * @return valid input for switch statement
+     */
+    public static int switchValidator(String prompt, int bound, Scanner scanscan) {
+        System.out.println(prompt);
+        while(true){
+            try {
+                int inty = Integer.parseInt(scanscan.next());
+                if (inty <= bound)
+                    return inty;
+                System.out.println("Invalid input.\n"+prompt);
+            } catch(NumberFormatException ne) {
+                System.out.println("Invalid f input.\n"+prompt);
+            }
+        }
+    }
+
+    /**
+     * @param os that string that we grab at the start of the runtime
+     */
+    public static void clear(String os) {
+        try {
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            System.out.println("Clear failed :( " + e);
+        }  
+    }
+
+}
